559af9040abf612f5e5282c7b593345b7412a5f2
Easton Gorishek <ieaston26@gmail.com>
2018-05-03 15:30:32 -0700

    trying again

--- app.js
+++ app.js
@@ -14,9 +14,32 @@ function randomWord(wordArray) {
     //this uses the random number to grab a word from our array
     var word = wordArray[index];
     console.log(word);
+
+    //convert word into an array
+    var newWordArray = word.split('');
+    console.log(newWordArray);
+
 }
 
 //this generates random word
 function getRandomIndex(max) {
     return Math.floor(Math.random() * Math.floor(max));
+}
+
+//this is the for loop to check if letter is in word
+
+
+var arrayValue = ['w', 'o', 'r', 'd'];
+
+function buttonPress() {
+    var userInput = document.getElementById('user-input').value;
+    console.log(userInput);
+    for(var i = 0; i < arrayValue.length; i++) {
+        if(userInput === arrayValue[i]) {
+            console.log(userInput);
+        }
+        else {
+            console.log('wrong');
+        }
+    }
 }
